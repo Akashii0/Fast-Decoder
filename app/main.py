c097c611eb7816103a3296ec17cd3d534fca8b84
Akashii0 <abdulkid151@gmail.com>
2024-10-23 20:48:31 +0100

    Updated main.py :D

--- app/main.py
+++ app/main.py
@@ -1,3 +1,7 @@
 from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
 
+from fastapi.templating import Jinja2Templates
 
+app = FastAPI()
